373c14305cfbde4bff5b4edb6977822eaceac628
Error273 <cfif12349@ya.ru>
2020-10-11 14:08:09 +0500

    Движение частиц во все 4 стороны

--- classes.py
+++ classes.py
@@ -1,4 +1,15 @@
+from constants import *
 from random import randint
+
+
+def pretty_print(map):
+    for i in map:
+        for j in i:
+            if j.__class__.__name__ == 'BaseCell':
+                print('-', end='')
+            else:
+                print('B', end='')
+        print()
 
 
 class BaseCell:
@@ -7,6 +18,7 @@ class BaseCell:
         self.x = x
         self.y = y
         self.color = color
+        self.updated = False
 
     def get_x(self):
         return self.x
@@ -26,6 +38,98 @@ class BaseCell:
     def set_color(self, color):
         self.color = color
 
-    def update(self):
-        self.color = (randint(0, 255), randint(0, 255), randint(0, 255))
+    def update(self, map, i, j):
+        return i, j
 
+    def set_updated(self, flag: bool):
+        self.updated = flag
+
+    def get_updated(self):
+        return self.updated
+
+
+class Bacteria(BaseCell):
+    def __init__(self, x, y, color):
+        super().__init__(x, y, color)
+        self.move = ''
+
+
+    def update(self, map1, i, j):
+        """Карта нужна для того, чтобы дать клеткам возможность смотреть вокруг.
+        i и j это координаты себя на этой самой карте. Вместо того, искать себя на этой карте двумя циклами,
+        мы один раз идем циклом в методе update у самой карты и выдаем всем клеткам их позиции.
+        Если клетка решила подвинуться, то двигаем ее в том же методе update у карты
+        Решение может быть немного сложное, но ничего лучшего я не придумал."""
+
+        # --- НЕЙРОНКА
+        move = self.move
+        # --- НЕЙРОНКА
+
+        if not self.updated:
+            self.updated = True
+            new_i, new_j = i, j
+
+            if move == 'right':
+                if j < len(map1[0]) - 1:
+                    new_j += 1
+                else:
+                    new_j = 0
+
+            elif move == 'left':
+                if j > 0:
+                    new_j -= 1
+                else:
+                    new_j = 59
+
+            elif move == 'down':
+                if i < 59:
+                    new_i += 1
+
+            elif move == 'up':
+                if i > 0:
+                    new_i -= 1
+
+            #  если в предполагаемой координате никого нет, то можем двигаться
+            if map1[new_i][new_j].__class__.__name__ == 'BaseCell':
+                self.x = 300 + new_j * CELL_SIZE
+                self.y = new_i * CELL_SIZE # координата рассчитывается на основе преполагаемого индекса
+                return new_i, new_j
+        return i, j
+
+
+class Map:
+    def __init__(self):
+        # создаем пустое поле с обычными фоновыми клетками
+        self.map_main = [[BaseCell(i, j, WHITE) for j in range(0, WINDOW_HEIGHT, CELL_SIZE)]
+                    for i in range(300, WINDOW_WIDTH, CELL_SIZE)]
+
+    def update(self):
+        """Для того, чтобы бактерии двигались более плавно, необходимо было ввести им параметр updated.
+        Без него получается так,что бактерии обновляются по несколько раз.
+        Потом, после того, как мы обновили все клетки, нужно им заного дать возможность обновиться."""
+        new_map = self.map_main[:]
+        for i in range(len(self.map_main)):
+            for j in range(len(self.map_main[0])):
+                cell = self.map_main[i][j]
+                new_i, new_j = cell.update(self.map_main[:], i, j)
+                # new_map[i].insert(new_j, new_map[i].pop(j))
+                new_map[i][j] = BaseCell(cell.x, cell.y, WHITE)
+                new_map[new_i][new_j] = cell
+        self.map_main = new_map
+
+        for line in self.map_main:
+            for cell in line:
+                cell.set_updated(False)
+
+    def set_cell(self, i, j, cell):
+        #  установить на какую-то позицию на карте клетку
+        self.map_main[i][j] = cell
+
+    def get_cell(self, i, j):
+        return self.map_main[i][j]
+
+    def get_map(self):
+        return self.map_main
+
+    def set_map(self, map):
+        self.map_main = map
--- main.py
+++ main.py
@@ -18,6 +18,9 @@ class Window(QMainWindow):
     def initUI(self):
         uic.loadUi('ui.ui', self)
 
+        #  устанавливаем фиксированный размер окна.
+        self.setFixedSize(WINDOW_WIDTH, WINDOW_HEIGHT)
+
         #  время, через которое обновляется экран, мс
         self.update_time = 200
 
@@ -29,16 +32,26 @@ class Window(QMainWindow):
 
         #  таймер необходим для того, чтобы правильно обновлять поле битвы. напрямую влияет на скорость симуляции.
         self.timer = QTimer()
-        self.timer.timeout.connect(self.repaint)
+        self.timer.timeout.connect(self.update)
         self.timer.start(self.update_time)
 
         # карта со всеми объектами типо бактрий или минералов. Пока заполняем пустыми клетками
         # сразу присвиваем клеткам x и y, отрисовывать потом будем именно по ним.
-        # 2D массив только для того, чтобы правильно задать координаты. Обращаться с ним неудобно, поэтому сразу
-        # же преобразуем в 1D
-        self.map = [[BaseCell(i, j, (255, 255, 255)) for j in range(0, WINDOW_HEIGHT, CELL_SIZE)]
-                    for i in range(300, WINDOW_WIDTH, CELL_SIZE)]
-        self.map = [item for sublist in self.map for item in sublist]
+        self.map = Map()
+
+        self.map.set_cell(10, 5, Bacteria(350, 100, (255, 255, 0)))
+        self.map.map_main[10][5].move = 'right'
+
+        self.map.set_cell(30, 5, Bacteria(350, 300, (0, 255, 255)))
+        self.map.map_main[30][5].move = 'left'
+
+        self.map.set_cell(20, 10, Bacteria(350, 400, (0, 255, 0)))
+        self.map.map_main[20][10].move = 'up'
+        self.map.set_cell(20, 20, Bacteria(350, 500, (0, 0, 255)))
+        self.map.map_main[20][20].move = 'down'
+
+
+
 
     def change_start_or_stop(self):
         if not self.started:
@@ -52,6 +65,7 @@ class Window(QMainWindow):
         self.update_time = self.simulation_speed_box.value()
         self.timer.start(self.update_time)
 
+
     def paintEvent(self, event):
         qp = QPainter()
         qp.begin(self)
@@ -59,23 +73,22 @@ class Window(QMainWindow):
         qp.end()
 
     def update_field(self, qp):
-
         # рисуем линию - ограничитель
         qp.setBrush(QColor(0, 0, 0))
         qp.drawLine(300, 0, 300, 600)
+        if self.started:
+            self.map.update()
+        # отрисовываем карту
+        map = self.map.get_map()
+        for i in map:
+            for cell in i:
+                if cell.__class__.__name__ != 'BaseCell':
+                    qp.setBrush(QColor(*cell.get_color()))
+                    #  qpainter по умлочанию рисует границу квадрата, если нужно чтобы появилась "сетка",
+                    #  закомментировать следующую строку
+                    qp.setPen(QPen(QColor(*cell.get_color())))
 
-        # размер одной клетки - 10 пикселей. Размер поля, на котором мы его будем рисовать (та часть, которая
-        # разделена линией) - 600 на 600. Получается, что размер поля 59 на 59 (один выходит за границы).
-        for cell in self.map:
-            # обновляем клетки только если не на паузе
-            if self.started:
-                cell.update()
-            qp.setBrush(QColor(*cell.get_color()))
-            #  qpainter по умлочанию рисует границу квадрата, если нужно чтобы появилась "сетка",
-            #  закомментировать следующую строку
-            qp.setPen(QPen(QColor(*cell.get_color())))
-
-            qp.drawRect(cell.get_x(), cell.get_y(), CELL_SIZE, CELL_SIZE)
+                    qp.drawRect(cell.get_x(), cell.get_y(), CELL_SIZE, CELL_SIZE)
 
 
 if __name__ == '__main__':
